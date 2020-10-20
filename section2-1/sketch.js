9e07a5436043b261a877d71be05ee91c4aa98654
IkedaEri <72751749+IkedaEri@users.noreply.github.com>
2020-10-20 11:39:26 +0900

    while文とfor文の練習

--- section2-1/sketch.js
+++ section2-1/sketch.js
@@ -1,15 +1,18 @@
 // テキスト「繰り返し」
 function setup() {
-  createCanvas(100,100);
+  createCanvas(200,200);
   fill(0);
   let x = 0;
   while(x < 100){ // 四角を描く座標が描画範囲内であれば続ける
     rect(x, x, 20, 20);
     x = x + 20; // 繰り返し1回ごとに場所を動かす
   }
+  for(let x = 100; x < 200; x = x + 20){
+fill(255, 204, 0);//黄色
+rect(x, x, 20, 20);//
+  }
 }
-
-// for 文を使うと、同じプログラムを以下のように書くことができます
+  // for 文を使うと、同じプログラムを以下のように書くことができます
 // function setup() {
 //   createCanvas(100,100);
 //   fill(0);
