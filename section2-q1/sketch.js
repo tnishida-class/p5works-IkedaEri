0acf87f72b75132076a8f2534acf8a4b47e3c67e
IkedaEri <72751749+IkedaEri@users.noreply.github.com>
2020-10-24 15:39:41 +0900

    section2-q別解

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,15 +1,15 @@
 // 小手調べ
 function setup(){
   createCanvas(200, 200);
-  noFill();
-   for(let i = 0; i < 10; i++){
-    if(i > 4){
+//  noFill();
+   for(let i = 10; i > 0; i--){
+    if(i > 5){
      stroke(255, 0, 0);
    }
     else{
      stroke(0, 0, 255);
    }
-  ellipse(100, 100, (1 + i) * 10)
+  ellipse(100, 100, i * 10)
     // BLANK[1]
   }
 }
