208fb51de28fc9c0942a61bab18e743c9cbf4e0c
IkedaEri <72751749+IkedaEri@users.noreply.github.com>
2020-10-24 14:30:07 +0900

    2－3偶数列の色を変える

--- section2-3/sketch.js
+++ section2-3/sketch.js
@@ -5,7 +5,7 @@ function setup(){
   createCanvas(100, 100);
   for(let i = 0; i < 10; i++){
     for(let j = 0; j < 10; j++){
-      fill(j % 2 == 1 ? 'rgba(0,255,0, 0.25)': 0); // ここを変えます
+      fill(i % 2 == 1 ? 'rgba(0,255,0, 0.25)': 0); // ここを変えます
       console.log(i, j);
       rect(i * 10, j * 10, 5, 5);
     }
