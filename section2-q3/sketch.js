d337114014b1d8be8b94551300b4e2ae5d792709
IkedaEri <72751749+IkedaEri@users.noreply.github.com>
2020-10-27 12:10:41 +0900

    section2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -19,7 +19,7 @@ function setup() {
   drawArcs(green, red, maxR * 0.8);
   drawArcs(cream, black, maxR * 0.75);
   drawArcs(green, red, maxR * 0.5);
-  drawArcs(cream, black, maxR * 0.45)
+  drawArcs(cream, black, maxR * 0.45);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
   drawCircle(green, maxR * 0.1);
   drawCircle(red, maxR * 0.05);
