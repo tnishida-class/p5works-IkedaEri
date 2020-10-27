0a80f470270acce78f4d42c400ef5581510d3b9b
IkedaEri <72751749+IkedaEri@users.noreply.github.com>
2020-10-27 12:10:09 +0900

    EUの旗

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -2,11 +2,25 @@
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background('rgb(0, 69, 181)');
+  angleMode(DEGREES);
   for(let i = 0; i < 12; i++){
-    let theta = TWO_PI * i / 12;
+    let theta = 360 * i / 12;
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    noStroke();
+    fill(255, 241, 0);
+    star(x, y, 10);
   }
+}
+
+function star(cx, cy, r){
+  beginShape();    // 点つなぎを始める
+  for(let i = 0; i < 5; i++){
+    const theta = 360 * i * 2 / 5 - 90;
+    const x = cx + cos(theta) * r;
+    const y = cy + sin(theta) * r;
+    vertex(x, y);  // 次につなぐ点を１つ増やす
+  }
+  endShape(CLOSE); // 点つなぎを終わる
 }
